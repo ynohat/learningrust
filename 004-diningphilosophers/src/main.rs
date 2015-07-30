2b501df16eb396a5bef842a145031a2b2ee8d508
Anthony Hogg <anthony@hogg.fr>
2015-07-30 23:39:20 +0200

    gives the thinkers forks and manners

--- 004-diningphilosophers/src/main.rs
+++ 004-diningphilosophers/src/main.rs
@@ -1,35 +1,57 @@
 use std::thread;
+use std::sync::{Mutex, Arc};
 
 struct Thinker {
     name: String,
+    idx: usize,
 }
 
 impl Thinker {
-    fn new(name: &str) -> Thinker {
+    fn new(name: &str, idx: usize) -> Thinker {
         Thinker {
             name: name.to_string(),
+            idx: idx,
         }
     }
 
-    fn eat(&self) {
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.idx]
+            .lock().unwrap();
+        let _right = table.forks[(self.idx + 1) % table.forks.len()]
+            .lock().unwrap();
         println!("{} tucks in.", self.name);
-        thread::sleep_ms(2000);
+        thread::sleep_ms(10000);
         println!("{} burps.", self.name);
     }
 }
 
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+
 fn main() {
     let thinkers = vec![
-        Thinker::new("Spinoza"),
-        Thinker::new("Deleuze"),
-        Thinker::new("Marx"),
-        Thinker::new("Nietzsche"),
-        Thinker::new("Foucault"),
+        Thinker::new("Spinoza", 0),
+        Thinker::new("Deleuze", 1),
+        Thinker::new("Marx", 2),
+        Thinker::new("Nietzsche", 3),
+        Thinker::new("Foucault", 4),
     ];
 
+    let table = Arc::new(Table {
+        forks: vec![
+            Mutex::new(()),
+            Mutex::new(()),
+            Mutex::new(()),
+            Mutex::new(()),
+            Mutex::new(()),
+        ]
+    });
+
     let handles: Vec<_> = thinkers.into_iter().map(|p| {
+        let table = table.clone();
         thread::spawn(move || {
-            p.eat();
+            p.eat(&table);
         })
     }).collect();
 
